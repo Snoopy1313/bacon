ccc795958942620e6c8e4560ac84527c6b999068
Marik <marik@king>
2026-03-16 14:25:13 +0200

    Add fetch from server form

--- bacon_package/server.py
+++ bacon_package/server.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 from bacon_package.bacon_distance import bacon_distance
+from flask_cors import CORS
 
 app = Flask("rpyc")
+CORS(app)
 
 
 @app.get("/get_bacon_distance/<source_id>/<target_id>")
-def get_bacon_distance(source_id: int, target_id: int):
-    return bacon_distance(source_id, target_id)
+def get_bacon_distance(source_id: str, target_id: str):
+    return str(bacon_distance(int(source_id), int(target_id)))
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=5000)
--- bacon_web/src/App.jsx
+++ bacon_web/src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  const [source, setSource] = useState("");
+  const [target, setTarget] = useState("");
+  const [distance, setDistance] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let myObject = await fetch(`http://localhost:5000/get_bacon_distance/${source}/${target}`);
+    let myText = await myObject.text();
+    setDistance(myText)
+  };
+
   return (
-    <p>A</p>
-  )
+      <form onSubmit={handleSubmit}>
+          <label>Actors ID</label>
+          <input
+            type="text"
+            value={source}
+            onChange={(e) => setSource(e.target.value)}
+            placeholder="Type something..."
+          />
+          <input
+            type="text"
+            value={target}
+            onChange={(e) => setTarget(e.target.value)}
+            placeholder="Type something..."
+          />
+        <button type="submit">
+          Submit
+        </button>
+        <label>Bacon distance: {distance}</label>
+      </form>
+    )
 }
 
 export default App
