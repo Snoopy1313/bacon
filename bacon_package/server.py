b991d284bf84b66a1714e957f329bb3e47a7bf36
Marik <marik@king>
2026-03-16 14:09:00 +0200

    Add run to server

--- bacon_package/server.py
+++ bacon_package/server.py
@@ -7,3 +7,6 @@ app = Flask("rpyc")
 @app.get("/get_bacon_distance")
 def get_bacon_distance(source_id: int, target_id: int):
     return bacon_distance(source_id, target_id)
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=5000)
