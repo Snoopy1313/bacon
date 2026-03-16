f58eab33b6f6b8ef0c7d38c7888b9306b2303f1f
Marik <marik@king>
2026-03-16 14:09:44 +0200

    Add vars to get_bacon_distance

--- bacon_package/server.py
+++ bacon_package/server.py
@@ -4,7 +4,7 @@ from bacon_package.bacon_distance import bacon_distance
 app = Flask("rpyc")
 
 
-@app.get("/get_bacon_distance")
+@app.get("/get_bacon_distance/<source_id>/<target_id>")
 def get_bacon_distance(source_id: int, target_id: int):
     return bacon_distance(source_id, target_id)
 
