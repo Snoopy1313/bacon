1e9da0b4440647dc7277f6287e83d086acf7ac50
Marik <marik@king>
2026-03-17 12:05:17 +0200

    Add good stuff

--- bacon_package/server.py
+++ bacon_package/server.py
@@ -1,20 +1,27 @@
-from flask import Flask, abort, request
+from flask import Flask, abort, jsonify
 from bacon_package.bacon_distance import bacon_distance, get_actor_id, initialize_connection
 from flask_cors import CORS
+from http import HTTPStatus
 
-app = Flask("rpyc")
+app = Flask("server")
 CORS(app)
 conn = initialize_connection()
 
 
 @app.get("/get_bacon_distance/<source_name>/<target_name>")
 def get_bacon_distance(source_name: str, target_name: str):
+    source_id = get_actor_id(source_name, conn)
+    if not source_id:
+        return jsonify(error="Bad Request", message=f"No such actor as: {source_name}"), 400
+
+    target_id = get_actor_id(target_name, conn)
+    if not target_id:
+        return jsonify(error="Bad Request", message=f"No such actor as: {target_name}"), 400
+    
     try:
-        source_id = get_actor_id(source_name, conn)[0]
-        target_id = get_actor_id(target_name, conn)[0]
-        return str(bacon_distance(source_id, target_id))
-    except LookupError:
-        return abort(400)
+        return jsonify(distance=str(bacon_distance(source_id[0], target_id[0]))), 200
+    except LookupError as e:
+        return jsonify(error="Bad Request", message=str(e)), 400
 
 
 if __name__ == "__main__":
--- bacon_web/src/App.jsx
+++ bacon_web/src/App.jsx
@@ -4,42 +4,43 @@ import './App.css'
 function App() {
   const [source, setSource] = useState("");
   const [target, setTarget] = useState("");
-  const [distance, setDistance] = useState("");
+  const [message, setMessage] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
-    let response = await fetch(`http://localhost:5000/get_bacon_distance/${source}/${target}`);
+    const response = await fetch(`http://localhost:5000/get_bacon_distance/${source}/${target}`);
+    const data = await response.json();
+    console.log(data)
     if (response.status == 400) {
-      setDistance("No such Actor")
+      setMessage(`${data.error}: ${data.message}`)
     }
     else if (response.status == 200) {
-      let myText = await response.text();
-      setDistance(myText)
+      setMessage("Bacon distance: " + data.distance)
     }
     else {
-      setDistance("Some error occurred")
+      setMessage("Some error occurred")
     }
   };
 
   return (
       <form onSubmit={handleSubmit}>
-          <label>Actors ID</label>
+          <label>Actors Bacon Distance Calculator</label>
           <input
             type="text"
             value={source}
             onChange={(e) => setSource(e.target.value)}
-            placeholder="Type something..."
+            placeholder="Type first actor..."
           />
           <input
             type="text"
             value={target}
             onChange={(e) => setTarget(e.target.value)}
-            placeholder="Type something..."
+            placeholder="Type second actor..."
           />
         <button type="submit">
           Submit
         </button>
-        <label>Bacon distance: {distance}</label>
+        <label>{message}</label>
       </form>
     )
 }
